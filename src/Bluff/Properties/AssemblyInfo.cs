acf0e3be901f64dd1fdbaeeaf7b948fed08743f2
Duane Newman <duane@alienarc.com>
2016-03-09 15:39:34 -0600

    Updating version number. Correcting License copyright.

--- src/Bluff/Properties/AssemblyInfo.cs
+++ src/Bluff/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bluff")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Extension commands for Sony Vegas Pro")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Alien Arc Technologies, LLC")]
 [assembly: AssemblyProduct("Bluff")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Alien Arc Technologies, LLC 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
